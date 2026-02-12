a8d3574d4fae6a1cfcf12e843610d46485f44b61
Heorhii Nadiradze <swain.volt@gmail.com>
2026-02-12 19:32:04 +0200

    fix and changes (Storage -> StorageModel and add ChatSessionModel)

--- alembic/env.py
+++ alembic/env.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-from models import Storage # noqa
+from models import StorageModel, ChatSessionModel # noqa
 from database import Base
 from settings import settings
 
