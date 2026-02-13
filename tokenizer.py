edc2789b5638bedba1c4818ea0cfea392c0bbf06
Heorhii Nadiradze <swain.volt@gmail.com>
2026-02-13 15:01:55 +0200

    add encoding_name to num_tokens_from_string

--- tokenizer.py
+++ tokenizer.py
@@ -1,7 +1,7 @@
 import tiktoken
 
 
-def num_tokens_from_string(string: str, encoding_name: str) -> int:
+def num_tokens_from_string(string: str, encoding_name: str = "o200k_base") -> int:
     """Returns the number of tokens in a text string."""
     encoding = tiktoken.get_encoding(encoding_name)
     num_tokens = len(encoding.encode(string))
