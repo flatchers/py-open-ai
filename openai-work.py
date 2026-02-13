1a347cd073b05d760f5d8598177a43d655e7146d
Heorhii Nadiradze <swain.volt@gmail.com>
2026-02-13 17:17:25 +0200

    change retrieve token & price information (from database)

--- openai-work.py
+++ openai-work.py
@@ -1,12 +1,11 @@
 import json
 import os
-from decimal import getcontext, Decimal
+from decimal import Decimal
 
 from fastapi import FastAPI, Depends
 from openai import AsyncOpenAI
 
 from dotenv import load_dotenv
-from pydantic import BaseModel
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
@@ -68,6 +67,23 @@ async def main_prompt(
     )
     answer = response.choices[0].message.content
     try:
+
+        stmt = (
+            select(ChatSessionModel)
+            .options(joinedload(ChatSessionModel.storages))
+            .where(ChatSessionModel.id == data.session_id)
+        )
+
+        result = await async_db.execute(stmt)
+        session = result.scalars().first()
+
+        request_price = Decimal("0.15")
+        response_price = Decimal("0.60")
+
+        for storage in session.storages:
+            if data.request == storage.request:
+                request_price = Decimal("0.075")
+
         new_data = json.loads(answer)
         new_storage = StorageModel(
             chat_session_id=data.session_id,
@@ -77,8 +93,23 @@ async def main_prompt(
         async_db.add(new_storage)
         await async_db.commit()
 
-        print("DATABASE STORAGE:", new_storage.chat_session_id, new_storage.request, new_storage.response)
-        print("TOKEN LEN: ", num_tokens_from_string(new_storage.request) + num_tokens_from_string(new_storage.response))
+        total_tokens = num_tokens_from_string(new_storage.request) + num_tokens_from_string(new_storage.response)
+        session.total_tokens += total_tokens
+        await async_db.flush()
+
+        price_for_a_request_token = Decimal(str(request_price / 1000000))
+        price_for_a_response_token = Decimal(str(response_price / 100000))
+        total_price = (
+            (
+                    price_for_a_request_token * Decimal(num_tokens_from_string(new_storage.request))
+            ) +
+            (
+                    price_for_a_response_token * Decimal(num_tokens_from_string(new_storage.response))
+            )
+        )
+        session.total_price += total_price
+        await async_db.commit()
+
         return {"ChatGPT": new_data}
     except json.JSONDecodeError:
         return {"error": "Response is not valid JSON", "raw_text": answer}
@@ -86,27 +117,20 @@ async def main_prompt(
 
 @app.post("/chat/history/{session_id}")
 async def show_session_history(session_id: int, async_db: AsyncSession = Depends(get_db)):
-    stmt = select(ChatSessionModel).options(joinedload(ChatSessionModel.storages)).where(ChatSessionModel.id == session_id)
+    stmt = (
+        select(ChatSessionModel)
+        .options(joinedload(ChatSessionModel.storages))
+        .where(ChatSessionModel.id == session_id)
+    )
     result = await async_db.execute(stmt)
     session = result.scalars().first()
 
     if session is None:
         return {"error": "Chat session not found"}
-    request_price = 0.15
-    response_price = 0.60
-    getcontext().prec = 12
-    price_for_a_request_token = Decimal(str(request_price / 1000000))
-    price_for_a_response_token = Decimal(str(response_price / 100000))
 
     return {
         "requests": [storage.request for storage in session.storages],
         "response": [storage.response for storage in session.storages],
-        "token_usage": sum(
-            [num_tokens_from_string(storage.request) for storage in session.storages] +
-            [num_tokens_from_string(storage.response) for storage in session.storages]
-        ),
-        "price": "$" + str(sum(
-            [price_for_a_request_token * Decimal(num_tokens_from_string(storage.request)) for storage in session.storages] +
-            [price_for_a_response_token * Decimal(num_tokens_from_string(storage.response)) for storage in session.storages]
-        ))
+        "token_usage": session.total_tokens,
+        "price_database": session.total_price
     }
