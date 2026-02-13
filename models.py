7bf8788617aa53885eb7d851f6046f88955849a8
Heorhii Nadiradze <swain.volt@gmail.com>
2026-02-13 17:12:14 +0200

    add total_tokens & total_price to ChatSessionModel

--- models.py
+++ models.py
@@ -1,7 +1,10 @@
-from sqlalchemy import Text, Integer, DateTime, ForeignKey
+from decimal import Decimal
+
+from sqlalchemy import Text, Integer, DateTime, ForeignKey, JSON, Numeric
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from database import Base
+from tokenizer import num_tokens_from_string
 
 
 class ChatSessionModel(Base):
@@ -9,7 +12,19 @@ class ChatSessionModel(Base):
 
     id = mapped_column(Integer, primary_key=True, autoincrement=True)
 
-    storages: Mapped["StorageModel"] = relationship("StorageModel", back_populates="session")
+    total_tokens: Mapped[int] = mapped_column(
+        Integer,
+        default=0,
+        nullable=False
+    )
+
+    total_price: Mapped[Decimal] = mapped_column(
+        Numeric(20, 12),
+        default=Decimal("0"),
+        nullable=False
+    )
+
+    storages: Mapped[list["StorageModel"]] = relationship("StorageModel", back_populates="chat_session")
 
 
 class StorageModel(Base):
@@ -18,6 +33,6 @@ class StorageModel(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     request: Mapped[str] = mapped_column(Text)
     response: Mapped[str] = mapped_column(Text)
-    chat_session: Mapped["ChatSessionModel"] = relationship("ChatSessionModel", back_populates="storage")
+    chat_session: Mapped["ChatSessionModel"] = relationship("ChatSessionModel", back_populates="storages")
 
-    chat_session_id: Mapped[int] = mapped_column(ForeignKey(ChatSessionModel.id, ondelete="CASCADE"))
+    chat_session_id: Mapped[int] = mapped_column(ForeignKey(ChatSessionModel.id, ondelete="CASCADE"), nullable=False)
